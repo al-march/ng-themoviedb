f00533bb53c059e81a9d0128d2896455832b31fc
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2020-03-03 01:30:23 +0300

    add header component

--- themoviedb/src/app/header/header.component.ts
+++ themoviedb/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +10,12 @@ export class HeaderComponent implements OnInit {
 
   @Output() openSettings = new EventEmitter();
 
-  constructor() { }
+  constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
   }
 
+  setAnotherRequest(value: string) {
+    this.movieService.setNewRequest(value)
+  }
 }
